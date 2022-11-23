debaa6f689912287a4470eb42282c326ff122546
moinshaikhpdx <mosh2@pdx.edu>
2022-11-24 02:44:54 +0530

    Updated the code structure

--- work/init.cpp
+++ work/init.cpp
@@ -3,44 +3,57 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
-
+#include <cstring>
 using namespace std;
+
+
 class cache_simulator
 {
-	uint16_t *tag;
-	uint8_t *lru;
-	uint8_t *mesi_bits;
-
-	int *operation;
-	int *addr;
-	int size;
-
-	cache_simulator(int init_size)
-	{
-		size = init_size;
-		tag = new uint16_t[size];
-	        lru = new uint8_t[size];
-		mesi_bits = new uint8_t[size];	
-	}
-
-	void read_trace(uint8_t *operation, string *addr, string PATH);// seperate instruction and operation
-  	void reset_cache();
-	void print_valid_cache();	
+	
+	struct {
+		short int PLRU; ///////////// LSB being the start of the root ////////// {d,e,f,g,b,c,a}////////////////// 
+		int line [8]; //////////// Tag array//////// {Messi bits[1:0],tagbits} ////////////// 
+	} cache[32768];
 
 
+	public:
 
+	void update_state(unsigned int state, unsigned int set, unsigned int way);	
+	int check_state(unsigned int state, unsigned int set, unsigned int way);
 };
 
+void cache_simulator::update_state(unsigned int state, unsigned int set, unsigned int way)
+{
+      cache[set].line[way] = (cache[set].line[way] & ~(3<<11)) | (state<<11);
+
+}
+
+int cache_simulator::check_state(unsigned int state, unsigned int set, unsigned int way)
+{
+	return (cache[set].line[way] >> 11);
+}
 
 int main(int argc, char* argv[])
 {
+
+   cache_simulator cache_sim;
+   cache_sim.update_state(3, 10, 2);
+   int my_state = cache_sim.check_state(3, 10, 2);
+   cout <<my_state;
+
    if(argc == 1)
    {
-	cout<<"Please provide trace path";
+	cout<<"Please provide trace path and mode";
    }
    else
    {
      string path = argv[1];
+     string mode = argv[2];
+
+ 
+
+     cout <<"\n\n\n Execution mode = "<<mode<<"\n\n\n";
+
      ifstream my_file(path);
      if(my_file.is_open())
      {
@@ -49,20 +62,34 @@ int main(int argc, char* argv[])
               {
                   istringstream iss(line);
                   string s;
+		  int command;
+		  string address;
                   int string_count = 0;
                   while (getline(iss, s, ' '))
                      {
                       if(string_count == 0)
                          {
-                            cout<<"Command : "<<s<<"\n";
+		            command = stoi(s);
                             string_count = 1;
                          }
                       else
                          {
-                            cout<<"Address : "<<s<<"\n";
+			   address = s;
                          }
                      }
+		      cout<<"Command = "<<command<<" Address = "<<address<<"\n";
 
+		      switch (command) {
+			      case 0: break;
+			      case 2: break;
+			      case 3: break;
+			      case 4: break;
+			      case 5: break;
+			      case 6: break;
+			      case 7: break;
+			      case 8: break;
+			      case 9: break;
+			}
                }
      }
      else
